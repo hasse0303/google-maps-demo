c23495a9ab08b2e1ff59277ebfa07d6ec1e1809b
Has Se <has.se@ecoinsoft.com>
2022-03-17 14:05:47 +0700

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { GoogleMapsModule } from '@angular/google-maps';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { GooglePlaceModule } from 'ngx-google-places-autocomplete';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GoogleMapsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    GooglePlaceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
